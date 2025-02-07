1a095cf2d4ba60bd06f4a544e6d5ecb0af9c9a7f
hgp22 <henriquepereira31337@gmail.com>
2025-02-07 21:45:05 +0000

    feat: add several cronjobs to check for posts and build the website

--- setjob.py
+++ setjob.py
@@ -5,15 +5,33 @@ import os
 python_bin = sys.executable
 
 script_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "main.py")
+website_build_command = "cp -f prices.csv /var/www/hgpereira/static/  && cd /var/www/hgpereira && sudo hugo -b https://www.hgpereira.com"
 
 if not os.access(script_path, os.X_OK):
     os.chmod(script_path, 0o755)
 
 cron = CronTab(user=True)
-job = cron.new(command=f'{python_bin} {script_path}', comment='Updates nos Precos dos Combustiveis')
 
-job.setall('0 20 * * 5')
+update_jobs = [
+    ('0 9-23 * * 5', 'Sexta Feira -- Verifica se saiu post'),
+    ('0 * * * 6', 'Sabado -- Verifica se saiu post'),
+    ('0 0-9 * * 7', 'Domigo -- Verifica se saiu post'),
+]
+
+for schedule, comment in update_jobs:
+
+    update_job = cron.new(command=f'{python_bin} {script_path}', comment=comment)
+    update_job.setall(schedule)
+
+    build_job = cron.new(command=website_build_command, comment=f'Build e update do .csv depois de {comment}')
+    
+    hour, minute, rest = schedule.split(" ", 2)
+    new_minute = (int(minute) + 5) % 60
+    new_hour = int(hour) + ((int(minute) + 5) // 60) if hour != '*' else '*'
+    new_schedule = f'{new_minute} {new_hour} {rest}'
+    build_job.setall(new_schedule)
 
 cron.write()
 
-print(f"Added: {job}")
+for j in cron:
+    print(f"Added: {j.comment} - {j}")
